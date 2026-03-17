2c31e400f3d080b8e92e6083b59ec6743ca461c5
eentks82137-dot <eentks82137@gmail.com>
2026-03-17 11:39:56 +0900

    upper, lower

--- day5/src/myMap.ts
+++ day5/src/myMap.ts
@@ -9,3 +9,11 @@ const nums2 = [10, 20, 309];
 
 const nums3 = nums2.map((n) => n * 3);
 console.log(nums3);
+
+const names = ['kim', 'lee'];
+const capName = names.map((s) => s.toUpperCase());
+console.log(capName);
+
+const caps = ['ASD', 'ASDEGW', 'GERASEDF', 'FQGRE', 'ASDB'];
+const lows = caps.map((n) => n.toLowerCase());
+console.log(lows);
